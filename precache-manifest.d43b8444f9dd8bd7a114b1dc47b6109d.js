19308dbd961b7a5ba5ae01aaa5e799e5707c3f5f
kyrie-eleison <econometrics4girls@gmail.com>
2020-10-18 19:58:53 +0900

    Updates

---
+++ precache-manifest.d43b8444f9dd8bd7a114b1dc47b6109d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "919f813adfbf630adfe89fa2756b68d6",
+    "url": "/kyoto/index.html"
+  },
+  {
+    "revision": "4c7714a2135cef32d237",
+    "url": "/kyoto/static/css/main.9fcd802d.chunk.css"
+  },
+  {
+    "revision": "f4fd6b62a05759efb391",
+    "url": "/kyoto/static/js/2.2c5e3a7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kyoto/static/js/2.2c5e3a7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c7714a2135cef32d237",
+    "url": "/kyoto/static/js/main.e1443f18.chunk.js"
+  },
+  {
+    "revision": "6a21e4f49e11c91dad68",
+    "url": "/kyoto/static/js/runtime-main.6706584c.js"
+  },
+  {
+    "revision": "8eadf828124e5387595faf687170a701",
+    "url": "/kyoto/static/media/streetMap.8eadf828.png"
+  }
+]);
