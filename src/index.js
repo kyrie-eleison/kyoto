4c836d6d126ac23c774eb09eca4b06eabf668d7b
kyrie-eleison <econometrics4girls@gmail.com>
2020-10-18 19:59:47 +0900

    for single page apps: https://create-react-app.dev/docs/deployment/

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './Index.css';
 import KyotoPicture from './banners/KyotoPicture.js';
 import Map from './banners/Map.js';
 import Contents from './banners/Contents.js';
@@ -21,17 +21,6 @@ class Index extends React.Component {
     }
 
     render(){
-
-        (function(l) {
-            if (l.search[1] === '/' ) {
-            var decoded = l.search.slice(1).split('&').map(function(s) { 
-                return s.replace(/~and~/g, '&')
-            }).join('?');
-            window.history.replaceState(null, null,
-                l.pathname.slice(0, -1) + decoded + l.hash
-            );
-            }
-        }(window.location))
 
         return(
             <React.Fragment>
