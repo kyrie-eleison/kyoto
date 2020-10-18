20f0935c39793e2b7ed756881110bac34a23f304
kyrie-eleison <econometrics4girls@gmail.com>
2020-10-18 19:24:01 +0900

    version up for single App deployment

--- src/index.js
+++ src/index.js
@@ -21,6 +21,18 @@ class Index extends React.Component {
     }
 
     render(){
+
+        (function(l) {
+            if (l.search[1] === '/' ) {
+            var decoded = l.search.slice(1).split('&').map(function(s) { 
+                return s.replace(/~and~/g, '&')
+            }).join('?');
+            window.history.replaceState(null, null,
+                l.pathname.slice(0, -1) + decoded + l.hash
+            );
+            }
+        }(window.location))
+
         return(
             <React.Fragment>
                 <KyotoPicture /> 
